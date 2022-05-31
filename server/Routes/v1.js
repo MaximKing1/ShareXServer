2e388f2081a6e0ff73df1baba383800191731b9a
MaximKing1 <73140396+MaximKing1@users.noreply.github.com>
2022-05-31 22:45:48 +0100

    Added File Saving Support

--- server/Routes/v1.js
+++ server/Routes/v1.js
@@ -1,15 +1,19 @@
 const { extname } = require('path');
 const { nameFunc } = require('./Functions/functionNameString');
+const { decode } = require('./Functions/decodeBase64');
+const { writeFile } = require('fs');
 
 module.exports = async function (app, opts, done) {
     app.post('/upload', function (req, res) {
-      let data = await req.file() 
+    const data = await req.file() 
     const name = nameFunc(9);
-    const file = data.file;
+    const file = decode(data.file);
     const ext = extname(data.filename);
     const dir = '../Uploads';
 
-      
+      writeFile(`${dir}/${name}${ext}`, file.data, function (err) { 
+        console.error(err)
+      });
   });
   done();
 };
